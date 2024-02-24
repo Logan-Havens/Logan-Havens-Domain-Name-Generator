cfd7f1036cbe3dddf02cedf067184426166141f3
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-02-24 01:58:33 +0000

    updated

--- src/app.js
+++ src/app.js
@@ -5,12 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// Write your code here
-console.log("Hello logan from the console!");
-
 let pronoun = ["the", "our", "my"];
 let adj = ["super", "ghostly", "dark", "crushing"];
-let noun = ["axe", "volleball", "athlete", "liger"];
+let noun = ["wallet", "volleball", "athlete", "liger"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
